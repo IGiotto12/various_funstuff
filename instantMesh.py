667e2fc8e1426373a0f9d58e2479d0e088a8850a
Jiaqi Wu <igiotto12@gmail.com>
2024-04-23 00:57:01 -0700

    Create instantMesh.py

---
+++ instantMesh.py
@@ -0,0 +1,14 @@
+import replicate
+
+output = replicate.run(
+    "camenduru/instantmesh:4f151757fd04d508b84f2192a17f58d11673971f05d9cb1fd8bd8149c6fc7cbb",
+    input={
+        "seed": 42,
+        "image_path": "URI",
+        "export_video": True,
+        "sample_steps": 75,
+        "export_texmap": True,
+        "remove_background": True
+    }
+)
+print(output)
