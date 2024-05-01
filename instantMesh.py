c1b090524c00f6ad4e9819fa2e30d01a763e6161
IGiotto <101021686+IGiotto12@users.noreply.github.com>
2024-05-01 12:45:03 -0700

    Update instantMesh.py

--- instantMesh.py
+++ instantMesh.py
@@ -4,8 +4,7 @@ output = replicate.run(
     "camenduru/instantmesh:4f151757fd04d508b84f2192a17f58d11673971f05d9cb1fd8bd8149c6fc7cbb",
     input={
         "seed": 42,
-        "image_path": "URI",
-        "export_video": True,
+        "image_path": "https://pngimg.com/d/question_mark_PNG134.png",
         "sample_steps": 75,
         "export_texmap": True,
         "remove_background": True
